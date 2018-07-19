88b7fafd219ea6ba5f784ca0497acd93b9acba54
Ozumii <xxryanbwxx@gmail.com>
2018-07-19 00:19:28 -0400

    updated test connection to allow for timeouts

--- test/create_test.js
+++ test/create_test.js
@@ -2,14 +2,22 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('creating records',()=>{
-    it('saves a user',()=>{
+    //call done here to test the assertion after the user has been saved
+    it('saves a user',(done)=>{
         //tests that a user is created
         //User here sets up the User collection
         //the name ryan creates a new document, nothing is saved in this statment
         const ryan = new User({name:'Ryan'});
 
         //this saves Ryan to the collection
-        ryan.save();
+        ryan.save()
+        //use a promise to confirm that this was saved
+        .then(()=>{
+            //confirm that user is actually saved to the collection
+            assert(!ryan.isNew);
+            done();
+            
+        });
        
     });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -3,21 +3,27 @@ const mongoose = require('mongoose');
 //local port number must be indicated or an error will be thrown
 const url ='mongodb://localhost:27017/users_test';
 
+//if promise is deprecated use es6 promises, mongoose newest version has this issue automatically
+
+//mongoose.Promise = global.Promise
+
+before((done)=>{
 //must be before tests
 //connecting to local database, this can be pointed internally or externally
 mongoose.connect(url,{useNewUrlParser:true});
-
-
 
 //start the connection
 mongoose.connection
 //if the connection is valid this looks like it uses Promises
 //Listen for mongo to emit open only one time
-.once('open', ()=>{ console.log('Connected to db!')})
+.once('open', ()=>{ done();})
 //on a failure, log the error
 //watch for mongo to emit an error and listen
 .on('error', (error)=>{
     console.warn('Warning',error);
+});
+
+    
 });
 
 //creating a hook to empty database when test is ran
